9a60c952002f9d0fbe378ee9cba1585015ddce9b
Samir Prajapati <sudarshanadhikari@SamirP-MacBook-Air-2.local>
2022-09-19 15:07:18 +0545

    added deliver script

--- scripts/jenkin_learning.sh
+++
@@ -1,5 +0,0 @@
-if [ $(docker inspect -f '{{.Name}}' jenkins-learning) = "/jenkins-learning" ]; 
-    then docker rm -f jenkins-learning; fi
-if [ ! -z "$(docker images -q jenkins-learning:latest)" ]; 
-    then docker rmi -f jenkins-learning:latest; fi
-docker compose -f docker-compose.stag.yml up -d
