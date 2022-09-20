b9c3357d896b845a045da8919b37ab7f9f6b17ea
Diwash Baral <diwas.baral@infodevelopers.com.np>
2022-09-20 16:05:24 +0545

    added nginx conf file

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -2,4 +2,4 @@ if [ $(docker inspect -f '{{.Name}}' jenkins-learning) = "/jenkins-learning" ];
     then docker rm -f jenkins-learning; fi
 if [ ! -z "$(docker images -q jenkins-learning:latest)" ]; 
     then docker rmi -f jenkins-learning:latest; fi
-docker compose -f docker-compose.stag.yml up -d
+docker compose -f docker-compose.yml up -d
